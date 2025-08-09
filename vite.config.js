4edfb7d3f048f5e7332d433fa11e137dece107e1
Andrew Liu <73562069+Dre1211-pixel@users.noreply.github.com>
2025-08-09 16:24:01 +0800

    更新 repository 名稱為 TravelersDepot

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 如果是 production 環境，設置正確的 base 路徑用於 GitHub Pages 部署
-  base: process.env.NODE_ENV === 'production' ? '/TravelerDepot/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/TravelersDepot/' : '/',
 })
